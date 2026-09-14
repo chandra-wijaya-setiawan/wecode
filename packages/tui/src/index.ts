5c94bb1ca71b35ab39e24999002780f5f49698d5
wecode <wecode@localhost>
2026-09-14 17:22:47 +0800

    draw-the-tree-as-an-outline-in-the-cockpit-add-p: attempt

--- packages/tui/src/index.ts
+++ packages/tui/src/index.ts
@@ -1,4 +1,16 @@
 export * from "./views.js";
 export * from "./list.js";
 export { App, boxKeys, type Screen } from "./app.js";
-export { BoxPage, Cockpit, Dashboard, KeyBar, Node } from "./screens.js";
+export { BoxPage, Cockpit, Dashboard, KeyBar, Node, Panel, COLUMNS } from "./screens.js";
+export {
+  foldedTo,
+  loadOutline,
+  nodeKey,
+  Outline,
+  OutlineError,
+  outlineRows,
+  OUTLINE,
+  rollup,
+  type OutlineConfig,
+  type OutlineLine,
+} from "./outline.js";
